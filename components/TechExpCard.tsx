ca790a1bc99c3c4ae26e656e4f8e822a2030a072
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2025-02-08 00:12:26 +0530

    Refactor TechExpCard component to improve type safety and simplify rendering logic

--- components/TechExpCard.tsx
+++ components/TechExpCard.tsx
@@ -1,38 +1,32 @@
 import dataset from "../db/techexpdata.json";
 
+type TechExpItem = {
+  Backend?: string[];
+  Frontend?: string[];
+  Database?: string[];
+  Tools?: string[];
+};
+
 export default function TechExpCard() {
-  // return Array(4)
-  //   .fill(0)
-  //   .map((_, index) => {
-  //     return (
-  //     <div className="w-3/4 space-y-5 shadow-md shadow-oliveGreen px-4 py-2 rounded-md" key={index}>
-  //       <h1 className="text-2xl py-2 px-2 font-bold pt-4">Backend</h1>
-  //       <div className="flex flex-wrap gap-5 pb-8">
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">API</span>
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">REST</span>
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">Django</span>
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">API</span>
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">API</span>
-  //         <span className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">API</span>
-  //       </div>
-  //     </div>
-  //     );
-  //   });
-
-  return dataset.map((items, index) => {
-
-    const keyname = Object.keys(items)
-      
+  return dataset.map((items: TechExpItem, index: number) => {
+    const keyname: keyof TechExpItem = Object.keys(items)[0] as keyof TechExpItem; 
+    
     return (
-            <div className="w-3/4 space-y-5 shadow-md shadow-oliveGreen px-4 py-2 rounded-md" key={index}>
+      <div
+        className="w-3/4 space-y-5 shadow-md shadow-oliveGreen px-4 py-2 rounded-md"
+        key={index}
+      >
         <h1 className="text-2xl py-2 px-2 font-bold pt-4">{keyname}</h1>
         <div className="flex flex-wrap gap-5 pb-8">
           {
-            items[keyname].map((value,index)=>  <span key={index} className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">{value}</span> )
+            items[keyname]?.map((value: string, index: number) => (
+              <span key={index} className="bg-deepBrown py-2 px-4 rounded-md h-fit w-fit">
+                {value}
+              </span>
+            ))
           }
         </div>
       </div>
-    )
-   
+    );
   });
 }
